979e87f33a502e222c26eee45eb5ed842df57b60
nooknpj <30778382+nooknpj@users.noreply.github.com>
2019-02-05 12:07:16 +0700

    add_fetch

--- src/pages/TestCss.js
+++ src/pages/TestCss.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import SearchBox from "../components/SearchBox";
 import CoursesBox from "../components/CoursesBox";
+import { Button } from "react-bootstrap";
 
 export class TestCss extends Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonStlye: 1
+    };
+  }
+
+  getButtonStyle = () => {
+    if (this.state.buttonStlye == 1) {
+      return {
+        backgroundColor: "blue"
+      };
+    } else {
+      return {
+        backgroundColor: "yellow"
+      };
+    }
+  };
   render() {
     return (
-      <div>
-        <p>blank page</p>
+      <div style={divStyle}>
+        <Button style={this.getButtonStyle} type="checkbox">
+          {" "}
+          testButton
+        </Button>
       </div>
     );
   }
 }
+
+const divStyle = {
+  padding: "50px"
+};
 const searchContainerStyle = {
   display: "flex",
   flexDirection: "row"
