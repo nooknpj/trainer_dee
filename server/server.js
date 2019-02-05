03530a742ced578fb21eaab4bf0c12940bc8438e
KNTXTremE <Kan.Horwang@gmail.com>
2019-02-05 16:26:37 +0700

    Edit filter_by_service (BUG)

--- server/server.js
+++ server/server.js
@@ -28,24 +28,84 @@ app.post('/trainer_dee/search_keyword', (req, res) => {
 });
 
 app.post("/trainer_dee/filter_by_service", (req, res) => {
-  let desiredFilters = new Set();
-  if (req.body["yoga"] == 1) desiredFilters.add(0);
-  if (req.body["cardio"] == 1) desiredFilters.add(1);
-  if (req.body["weightTraining"] == 1) desiredFilters.add(2);
+  let desiredFilters = []
+  let sql = ""
+  if (req.body["yoga"] == 1) desiredFilters.push(0);
+  if (req.body["cardio"] == 1) desiredFilters.push(1);
+  if (req.body["weightTraining"] == 1) desiredFilters.push(2);
 
   console.log(desiredFilters);
 
-  let sql =
-    "select * from course c where c.CourseID = (select CourseID FROM course c where c.Service = 0)";
-
-  connection.query(sql, req.body["Service"], (error, result) => {
-    if (error) throw error;
-    // let list_result = [];
-
-    let all = JSON.parse(JSON.stringify(result));
-    res.send(all);
-    //console.log(all);
-  });
+  if(desiredFilters.length == 0){
+      res.send([]);
+  }else if(desiredFilters.length == 1){
+      console.log("Case filtered 1")
+      sql = "select * from course c where c.CourseID = (select CourseID FROM course c where c.Service = ?)";
+      connection.query(sql, [desiredFilters[0]], (error, result) => {
+        if (error) throw error;
+        // let list_result = [];
+    
+        let all = JSON.parse(JSON.stringify(result));
+        res.send(all);
+        //console.log(all);
+      });
+    } else if(desiredFilters.length == 2){
+      console.log("Case filtered 2")
+      sql = "select * from course c where c.CourseID = (select CourseID FROM course c where c.Service = ? or c.Service = ?)";
+      connection.query(sql, [desiredFilters[0], desiredFilters[1]], (error, result) => {
+        if (error) throw error;
+    
+        let all = JSON.parse(JSON.stringify(result));
+        res.send(all);
+        //console.log(all);
+      });
+    } else if(desiredFilters.length == 3){
+      console.log("Case filtered 3")
+      sql = "select * from course c";
+      connection.query(sql, (error, result) => {
+        if (error) throw error;
+    
+        let all = JSON.parse(JSON.stringify(result));
+        res.send(all);
+        //console.log(all);
+      });
+  }
+  // switch(desiredFilters.length){
+  //   case 0: 
+  //     res.send([]);
+  //     break
+  //   case 1:
+  //     sql = "select * from course c where c.CourseID = (select CourseID FROM course c where c.Service = ?)";
+  //     connection.query(sql, desiredFilters[0], (error, result) => {
+  //       if (error) throw error;
+  //       // let list_result = [];
+    
+  //       let all = JSON.parse(JSON.stringify(result));
+  //       res.send(all);
+  //       //console.log(all);
+  //     });
+  //     break
+  //   case 2:
+  //     sql = "select * from course c where c.CourseID = (select CourseID FROM course c where c.Service = ? or c.Service = ?)";
+  //     connection.query(sql, desiredFilters[0], desiredFilters[1], (error, result) => {
+  //       if (error) throw error;
+    
+  //       let all = JSON.parse(JSON.stringify(result));
+  //       res.send(all);
+  //       //console.log(all);
+  //     });
+  //     break
+  //   case 3:
+  //     sql = "select * from course c";
+  //     connection.query(sql, (error, result) => {
+  //       if (error) throw error;
+    
+  //       let all = JSON.parse(JSON.stringify(result));
+  //       res.send(all);
+  //       //console.log(all);
+  //     });
+  //     break
+  // }
 });
 
 app.post("/trainer_dee/filter_by_gender", (req, res) => {
