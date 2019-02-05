8c03b04520429d1aa26d42775af36a02a0bb458c
KNTXTremE <Kan.Horwang@gmail.com>
2019-02-05 16:52:43 +0700

    Cleanup

--- server/server.js
+++ server/server.js
@@ -34,12 +34,9 @@ app.post("/trainer_dee/filter_by_service", (req, res) => {
   if (req.body["cardio"] == 1) desiredFilters.push(1);
   if (req.body["weightTraining"] == 1) desiredFilters.push(2);
 
-  console.log(desiredFilters);
-
   if(desiredFilters.length == 0){
       res.send([]);
   }else if(desiredFilters.length == 1){
-      console.log("Case filtered 1")
       sql = "select * from course c where c.Service = ?";
       connection.query(sql, [desiredFilters[0]], (error, result) => {
         if (error) throw error;
@@ -49,7 +46,6 @@ app.post("/trainer_dee/filter_by_service", (req, res) => {
         //console.log(all);
       });
     } else if(desiredFilters.length == 2){
-      console.log("Case filtered 2")
       sql = "select * from course c where c.Service = ? or c.Service = ?";
       connection.query(sql, [desiredFilters[0], desiredFilters[1]], (error, result) => {
         if (error) throw error;
@@ -59,7 +55,6 @@ app.post("/trainer_dee/filter_by_service", (req, res) => {
         //console.log(all);
       });
     } else if(desiredFilters.length == 3){
-      console.log("Case filtered 3")
       sql = "select * from course c";
       connection.query(sql, (error, result) => {
         if (error) throw error;
