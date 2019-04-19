a90dbf6cdc274fc9425fe3ce91f9a88ad535b74f
nooknpj <30778382+nooknpj@users.noreply.github.com>
2019-04-19 15:53:25 +0700

    minor changes on footer
    minor changes on footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,16 +4,17 @@ export class Footer extends Component {
   render() {
     return (
       <div style={FooterStyle}>
-        <a className="FooterLink" href="/">
+        {/* <a className="FooterLink" href="/">
           Home
         </a>{" "}
         |{" "}
         <a className="FooterLink" href="/searchCourses">
           Search Courses
-        </a>
+        </a> */}
         <div style={FooterDplopBox}>
           <a id="footerDplop">Trainer D By D'Plop</a>
         </div>
+        <p style={{ fontSize: "1.2em" }}> Contact: D.plop4@gmail.com</p>
         <p> © Copyright. 2019 D'Plop Company. All rights reserved.</p>
       </div>
     );
