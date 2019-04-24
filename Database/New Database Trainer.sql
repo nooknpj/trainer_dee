843472ad320b9c7aac10f096c90bf67bf6404a0c
Teeranat <31313586+DDEATHQQb@users.noreply.github.com>
2019-04-24 09:49:07 +0700

    without dump data

--- Database/New Database Trainer.sql
+++ Database/New Database Trainer.sql
@@ -64,8 +64,7 @@ create table TimeTable (
             delete cascade on
             update cascade
 );
-insert into TimeTable(tableClientID , startTime,endTime , tableStatus) 
-value  ('0000000000' , '2019-01-01 10:00:00' , '2019-01-01 12:00:00' , 'reserved');
+
 create table Location (
   LocationID int(10) auto_increment,
   LocateCourseID int (10),
